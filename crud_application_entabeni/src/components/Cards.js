73f0d8d180dea2f4101595bbaa3ebd57c7c1f88d
SaiSrinivas1010 <saikeesari2000@gmail.com>
2023-02-06 22:19:47 -0500

    Delete functionality working

--- crud_application_entabeni/src/components/Cards.js
+++ crud_application_entabeni/src/components/Cards.js
@@ -2,10 +2,22 @@ import React from 'react';
 import SkiDetails from './SkiDetails';
 import { FaLocationArrow, FaSkiing, FaPen, FaTrash} from "react-icons/fa";
 import './cards.css'
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Cards = () => {
+
+    let history = useNavigate();
+
+    const handleDelete = (id) =>{
+        var index = SkiDetails.map(function(e){
+            return e.id
+        }).indexOf(id);
+
+        SkiDetails.splice(index,1);
+        history('/');
+    }
 
     return(
         <section className='skilist'>
@@ -18,7 +30,7 @@ const Cards = () => {
                 <p><FaSkiing/> {SkiItem.ski_runs}</p>
                 <span className="udbuttons">
                     <button><FaPen/></button>
-                    <button ><FaTrash/></button>
+                    <button onClick={() => handleDelete(SkiItem.id)}><FaTrash/></button>
                 </span>
             </article>
                 );
